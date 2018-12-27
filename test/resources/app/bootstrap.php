44cd7c33d07da8503cf66c25c76eedbf598c2640
Mehmet Korkmaz <mehmet@mkorkmaz.com>
2018-12-27 11:41:34 +0300

    Test refactored for basic functionality

--- test/resources/app/bootstrap.php
+++ test/resources/app/bootstrap.php
@@ -2,36 +2,15 @@
 declare(strict_types=1);
 
 chdir(__DIR__);
-require '../../../vendor/autoload.php';
-$dotenv = new Dotenv\Dotenv(__DIR__);
-$dotenv->load();
-$container = include './config/container.php';
+require __DIR__ . '/../../../vendor/autoload.php';
+$container = include __DIR__ . '/config/container.php';
 
 use Zend\HttpHandlerRunner\Emitter\EmitterStack;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
-use Zend\HttpHandlerRunner\RequestHandlerRunner;
-
 use Zend\Diactoros\ServerRequestFactory;
-use Zend\Diactoros\Response;
-use Zend\Diactoros\ServerRequest;
-use Zend\Stratigility\Middleware\ErrorResponseGenerator;
-
-$errorResponseGenerator = function (Throwable $e) {
-    $generator = new ErrorResponseGenerator();
-    return $generator($e, new ServerRequest(), new Response());
-};
-
 $serverRequestFactory = [ServerRequestFactory::class, 'fromGlobals'];
-
 $stack = new EmitterStack();
 $stack->push(new SapiEmitter());
-
 $myApp = Selami\Application::createWithContainer($container, 'selami');
-
-$runner = new RequestHandlerRunner(
-    $myApp,
-    $stack,
-    $serverRequestFactory,
-    $errorResponseGenerator
-);
-$runner->run();
+$response = $myApp->handle($serverRequestFactory());
+$stack->emit($response);
--- test/resources/app/config/config.php
+++ test/resources/app/config/config.php
@@ -11,6 +11,8 @@ $config = [];
 if (is_file($cachedConfigFile)) {
     return require $cachedConfigFile;
 }
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+$dotenv->load();
 foreach (Glob::glob(__DIR__ . '/autoload/{{,*.}global,{,*.}local}.php', Glob::GLOB_BRACE) as $file) {
     $config = ArrayUtils::merge($config, include $file);
 }
